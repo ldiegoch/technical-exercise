5eb9078f4d3319f3e4a88c3759b7300c0008f9ad
Luis Diego Cordero <ldiegoch@gmail.com>
2019-03-07 15:24:36 -0600

    Initialize the repository with the project provided

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          alignSelf: "center",
+          display: "flex"
+        }}
+      >
+        <code>
+          TODO: build single page application that searches a photo API and
+          displays the results
+        </code>
+      </div>
+    );
+  }
+}
+
+export default App;
