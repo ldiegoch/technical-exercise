b9891d7c71e045806ddd3c7c7ccd42293ea0d0b4
Luis Diego Cordero <ldiegoch@gmail.com>
2019-03-07 20:45:02 -0600

    Implement StackGrid and Image component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
-import SearchBar from './components/search-bar/SearchBar'
+import SearchBar from './components/search-bar/SearchBar';
+import StackGrid, { transitions, easings } from "react-stack-grid";
+import Image from "./components/image/Image";
+import photos from './temp/photos';
 
 class App extends Component {
+  gutterWidth = 20;
+  gutterHeight=20;
+  columnWidth=300;
   render() {
     return (
       <div>
         <SearchBar/>
+        <StackGrid
+          columnWidth={this.columnWidth}
+          gutterWidth={this.gutterWidth}
+          gutterHeight={this.gutterHeight}
+        >
+          {photos.map(obj => (
+            <Image
+              src={obj.src}
+              label={obj.label}
+              height={300}
+              width={300}
+              image='https://media0.giphy.com/media/feqkVgjJpYtjy/200w.gif'
+              stillImage={obj.src}
+              originalImage="http://media0.giphy.com/media/feqkVgjJpYtjy/giphy.gif"
+              originalWidth='300'
+              originalHeight='300'
+            />
+          ))}
+        </StackGrid>
       </div>
     );
   }
---
+++ src/components/image/Image.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import styles from './Image.css';
+
+class Image extends Component {
+  state = {
+    hover: false,
+  };
+
+  handleOnMouseEnter = () => {
+    this.setState({ hover: true });
+  }
+
+  handleOnMouseLeave = () => {
+    this.setState({ hover: false });
+  }
+
+  render () {
+    const src = this.state.hover ? this.props.image : this.props.stillImage;
+    return (
+      <div
+        className='image'
+        onMouseEnter={this.handleOnMouseEnter}
+        onMouseLeave={this.handleOnMouseLeave}
+      >
+        <img
+          className='image__img--rounded'
+          src={src}
+          alt={this.props.label}
+          height={this.props.height}
+          width={this.props.width}
+        />
+        <div className='image__label'>{this.props.label}</div>
+      </div>
+    );
+  }
+}
+
+export default Image;
